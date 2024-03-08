16ee9da7b007ef6263f6ea4ae9c9a851ab5c99db
dimentiygg <sfx670@gmail.com>
2024-03-08 23:07:39 +0200

    sliders

--- java.js
+++ java.js
@@ -1,39 +1,87 @@
-const r = document.querySelector('.splidez')
+function switchClass() {
+  if (window.innerWidth > 767) {
+    return 3;
+  } else {
+    return 1;
+  }
+}
+
+function PerMove() {
+  if (window.innerWidth > 767) {
+    return 5;
+  } else {
+    return 1;
+  }
+}
+
+const r = document.querySelector('.splidez');
+const splideSection = document.querySelectorAll('.splide');
 document.addEventListener('DOMContentLoaded', function () {
-  var splide = new Splide('.splide', {
+  var optionsSplide = {
     type: 'slide',
     focus: 'center',
-    perPage: 1,
-    perMove: 1,
-  }).mount();
+    perPage: switchClass(),
+    perMove: PerMove(),
+  };
 
+  var splide = new Splide('.splide', optionsSplide).mount();
 
+  var optionsSplide1 = Object.assign({}, optionsSplide, {
+    perPage: switchClass(),
+    perMove: PerMove(),
+  });
 
-  var splide = new Splide('#slider1', {
-    type: 'slide',
-    focus: 'center',
-    perPage: 1,
-    perMove: 1,
-  }).mount();
+  var splide1 = new Splide('#slider1', optionsSplide1).mount();
 
+  var optionsSplide2 = Object.assign({}, optionsSplide, {
+    perPage: switchClass(),
+    perMove: PerMove(),
+  });
 
-  var splide = new Splide('#slider2', {
-    type: 'slide',
-    focus: 'center',
-    perPage: 1,
-    perMove: 1,
+  var splide2 = new Splide('#slider2', optionsSplide2).mount();
 
-  }).mount();
+  var optionsSplide3 = Object.assign({}, optionsSplide, {
+    perPage: switchClass(),
+    perMove: PerMove(),
+    height: '15rem',
+  });
 
+  var splide3 = new Splide('#slider3', optionsSplide3).mount();
 
-  var splide = new Splide('#slider3', {
-    type: 'slide',
-    focus: 'center',
-    perPage: 1,
-    perMove: 1,
-    height:'15rem'
-  }).mount();
+  console.log(splide3);
 
+  if (window.innerWidth > 1157) {
+    for (const splide of splideSection) {
+      splide.style.display = 'none';
+    }
+  }
+
+  window.addEventListener('resize', function () {
+    if (window.innerWidth < 1157) {
+      optionsSplide.perPage = switchClass();
+      optionsSplide.perMove = switchClass();
+      splide.destroy();
+      splide = new Splide('.splide', optionsSplide).mount();
+
+      optionsSplide1.perPage = switchClass();
+      optionsSplide1.perMove = switchClass();
+      splide1.destroy();
+      splide1 = new Splide('#slider1', optionsSplide1).mount();
+
+      optionsSplide2.perPage = switchClass();
+      optionsSplide2.perMove = switchClass();
+      splide2.destroy();
+      splide2 = new Splide('#slider2', optionsSplide2).mount();
+
+      optionsSplide3.perPage = switchClass();
+      optionsSplide3.perMove = switchClass();
+      splide3.destroy();
+      splide3 = new Splide('#slider3', optionsSplide3).mount();
+    }
+    if (window.innerWidth > 1157) {
+      for (const splide of splideSection) {
+        splide.style.display = 'none';
+      }
+    }
+  });
 });
-
-
